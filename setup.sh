ba6e83f9ac2a3658e60dcd2c50cb124c84f133bb
Dylan Dean <dylancvdean@gmail.com>
2023-02-27 21:11:07 -0700

    more python deps

--- setup.sh
+++ setup.sh
@@ -24,7 +24,7 @@ sudo apt-get -y install build-essential git libncurses-dev flex bison openssl \
   liblz4-tool bc curl gcc git libssl-dev libncurses5-dev lzop make \
   unzip exfat-utils rsync dosfstools fdisk python3 python3-pip python-is-python3
 
-pip install spdx_lookup pybind11
+pip install spdx_lookup pybind11 cppy
 pushd build
 ## Linux kernel
 if [ ! -d linux-socfpga ]
