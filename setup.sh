e4057b64d381afe245c822e626df05630cd7ab26
Dylan Dean <dylancvdean@gmail.com>
2023-03-01 09:08:15 -0700

    more

--- setup.sh
+++ setup.sh
@@ -28,10 +28,10 @@ sudo apt-get -y install build-essential git libncurses-dev flex bison openssl \
   libreadline-dev libgdbm-dev libdb-dev tk-dev libssl-dev libffi-dev \
   libpng-dev libjpeg-dev libgif-dev gcc-aarch64-linux-gnu \
   liblzma-dev libncursesw5-dev libtinfo-dev \
-  gdb lcov pkg-config \
+  gdb lcov pkg-config python3-setuptools \
   libbz2-dev libffi-dev libgdbm-dev libgdbm-compat-dev liblzma-dev \
   libncurses5-dev libreadline6-dev libsqlite3-dev libssl-dev \
-  lzma lzma-dev tk-dev uuid-dev zlib1g-dev
+  lzma lzma-dev tk-dev uuid-dev zlib1g-dev python-setuptools
 
 ## Python packages
 pip install spdx_lookup pybind11 cppy
